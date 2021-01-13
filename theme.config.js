1eae5debfe49ea9ac2f68618bc692204f9aabf1c
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-01-13 11:00:52 -0700

    ✨ Update footer

--- theme.config.js
+++ theme.config.js
@@ -2,7 +2,7 @@ const YEAR = (new Date).getFullYear()
 
 export default {
   footer: <small style={{ display: 'block', marginTop: '8rem' }}>
-    <abbr title='This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.' style={{cursor: 'help'}}>CC BY-NC 4.0</abbr> <time>{YEAR}</time> © Shu Ding.
+    <time>{YEAR}</time> © Luis Angel Ortega.
     <a href="/feed.xml">RSS</a>
     <style jsx>{`
       a {
