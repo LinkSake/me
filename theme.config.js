3634b40b4fccc33421d1bf649f267ba490455862
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-01-14 10:33:41 -0700

    🎨 Update footer

--- theme.config.js
+++ theme.config.js
@@ -2,7 +2,7 @@ const YEAR = (new Date).getFullYear()
 
 export default {
   footer: <small style={{ display: 'block', marginTop: '8rem' }}>
-    <time>{YEAR}</time> © Luis Angel Ortega.
+    Luis Angel Ortega © <time>{YEAR}</time>
     <a href="/feed.xml">RSS</a>
     <style jsx>{`
       a {
