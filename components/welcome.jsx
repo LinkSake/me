2d28eb2e71fd3544ad966dffb1adf45d967c90d1
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-01-21 16:49:27 -0700

    💄 Made Welcome component mobile friendly

--- components/welcome.jsx
+++ components/welcome.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Welcome = ( props ) => {
     return (
-        <div className='flex items-center'>
-            <div className='flex-column'>
+        <div className='welcome'>
+            <div className='flex-column welcome-img-container'>
                 <img className='welcome' src={props.img} alt={props.title}/>
             </div>
             <div className='flex-column'>
