2f38ff89f1bb8d1c9cc97354806b81e172147687
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-08-10 11:38:05 -0600

    💄 Update welcome page style

--- components/welcome.jsx
+++ components/welcome.jsx
@@ -6,7 +6,7 @@ const Welcome = ( props ) => {
             <div className='flex-column welcome-img-container'>
                 <img className='welcome' src={props.img} alt={props.title}/>
             </div>
-            <div className='flex-column'>
+            <div className='flex-column welcome-text '>
                 <h1 className='flex-row'>{props.title}</h1>
                 <span className='flex-row'>{props.children}</span>
             </div>
